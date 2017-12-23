c9c891bcc66aa6ca769aaf25a8ae89e3f618007a
monkwoo <mayankvijaypathak@gmail.com>
2017-12-23 23:40:09 +0530

    api calibrations

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,8 +5,8 @@ import FILTER_LIST from './filter-list-schema';
 import PRODUCT_LIST from './product-list-schema';
 
 // var __product_api_url = "/getProducts";
-var __product_api_url = "/getProducts";
-var __filters_api_url = "/getFilters";
+var __product_api_url = "/es/filter/furniture";
+var __filters_api_url = "/es/filter";
 
 var __fallback = (resolve, DATA, TIMEOUT) => setTimeout(
   resolve.bind(null, DATA), TIMEOUT
@@ -17,7 +17,10 @@ export function getFilterList(){
   return new Promise(
     resolve => {
       if(__filters_api_url){
-        axios.get(__filters_api_url).then(
+        axios.get(
+          __filters_api_url,
+          { params: { query: "furniture" } }
+        ).then(
           response => resolve(response.data)
         ).catch(
           __fallback.bind(
@@ -33,23 +36,22 @@ export function getFilterList(){
   );
 }
 
-export function getProductByFilters(filter_data){
+export function getProductByFilters(query){
   return new Promise(
     resolve => {
       if(__product_api_url){
         axios.post(
-          __product_api_url,
-          filter_data
+          __product_api_url, { query }
         ).then(
           response => resolve(response.data)
         ).catch(
           __fallback.bind(
-            null, resolve, PRODUCT_LIST(filter_data), _.random(2500, 3000)
+            null, resolve, PRODUCT_LIST(query), _.random(2500, 3000)
           )
         )
       }else{
         __fallback(
-          resolve, PRODUCT_LIST(filter_data), _.random(2500, 3000)
+          resolve, PRODUCT_LIST(query), _.random(2500, 3000)
         );
       }
     }
